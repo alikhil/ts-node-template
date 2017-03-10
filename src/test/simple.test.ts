a76bf401cd66c8d5f2e15fd6372b0b4051ce41f0
Alik Khilazhev <alikhil@mail.ru>
2017-03-10 17:03:43 +0300

    Adds tests

---
+++ src/test/simple.test.ts
@@ -0,0 +1,25 @@
+import * as mocha from 'mocha';
+import * as chai from 'chai';
+import chaiHttp = require("chai-http");
+
+import app from ".././app";
+
+chai.use(chaiHttp)
+const expect = chai.expect
+
+describe("base route", () => {
+
+    it("should be json", () => {
+        return chai.request(app).get("/")
+            .then((res) => {
+                expect(res.type).to.eql("application/json");
+            });
+    });
+
+    it("should have a message field", () => {
+        return chai.request(app).get("/")
+            .then((res) => {
+                expect(res.body.message).to.eql("Hello World!");
+            });
+    });
+});
