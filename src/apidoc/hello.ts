a0e524bb030929b0cb74fc1fafd855ee2fed9eb4
Alik Khilazhev <alikhil@mail.ru>
2017-07-04 20:11:23 +0300

    Add apidoc
    * dependecies
    * example in src/apidoc

---
+++ src/apidoc/hello.ts
@@ -0,0 +1,11 @@
+/**
+ * @apiVersion 0.0.1
+ * @apiGroup Common
+ * @api {get} hello Hello, world!
+ * @apiDescription Return "Hello, world!" message
+ * @apiSuccessExample {json} Success-Example
+ * {
+ *      "message": "Hello, world!"
+ * }
+ * @apiSuccess {String} message Message from controller
+ */
